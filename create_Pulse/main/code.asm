bbd310da776f5070fe06c5d104839187ced21c1b
Danh Phan <phandanh2112@gmail.com>
2023-09-04 17:17:52 +0700

    create_Pulse - ver 7.0 - freq 100Hz, duty cycle 50%

---
+++ create_Pulse/main/code.asm
@@ -0,0 +1,33 @@
+	ORG 00H				 	; reset
+	LJMP MAIN			 
+	ORG 0BH				   	; timer0 interrupt
+	LJMP TIMER_INT0			
+	ORG 30H	
+						
+
+
+MAIN:	
+	MOV IE, #82H			; enable global, timer0 interrupt
+
+	MOV TMOD, #01H			; timer0, mode 16-bit
+	MOV TL0, #LOW(-5000)	; 100Hz <-> 10,000us 
+	MOV TH0, #HIGH(-5000)
+	SETB TR0				; start timer0
+
+	SJMP $					; while(1)
+
+
+													
+TIMER_INT0:
+	CPL P1.0				; toggle
+	
+	CLR TF0					; clear overflow flag		
+	MOV TL0, #LOW(-5000)	; 5,000us
+	MOV TH0, #HIGH(-5000)
+	SETB TR0		 		; start timer0
+						
+	RETI
+
+
+
+END								  	
